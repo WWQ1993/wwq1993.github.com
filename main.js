2af1f77b50a6579ca800fdea2af4fd35b530c3e6
WWQ1993 <516041710@qq.com>
2015-07-08 21:51:26 +0800

    update

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var pa=/\[bc\]at/i;
-console.log(pa.global);
+var text = "0000mom and dad and baby";
+var pattern = /mom( and dad( and baby)?)?/gi;
+var matches = pattern.exec(text);
+console.log(matches.index); //
+console.log(matches.input); // "mom and dad and baby"
+console.log(matches[0]); // "mom and dad and baby"
+console.log(matches[1]); // " and dad and baby"
+console.log(matches[2]); // " and baby"
