3c1e1211e6d0447f2a44ba68175df42a0a2dd0f1
WWQ1993 <516041710@qq.com>
2016-02-22 00:06:27 +0800

    todo

--- js/common.js
+++ js/common.js
@@ -5,8 +5,8 @@ define(function (require, exports, module) {
     var $ = require('js/jquery.js');
 
     var component = {
-        header:$('header'),
-        nav:$('nav')
+        header: $('header'),
+        nav: $('nav')
     }
 
     exports.init = function () {
@@ -15,28 +15,26 @@ define(function (require, exports, module) {
 
     };
 
-    function IsPC() {
-        var userAgentInfo = navigator.userAgent;
-        var Agents = ["Android", "iPhone",
-            "SymbianOS", "Windows Phone",
-            "iPad", "iPod"];
-        var flag = true;
-        for (var v = 0; v < Agents.length; v++) {
-            if (userAgentInfo.indexOf(Agents[v]) > 0) {
-                flag = false;
-                break;
-            }
-        }
-        if(!flag){
-            flag = Math.min($(window).width(), $(window).height())>=600?true:false;
-        }
-        return flag;
-    }
-
     function setHtmlFontSize() {
+        var IsPC = function () {
+            var userAgentInfo = navigator.userAgent;
+            var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+            var flag = true;
+            for (var v = 0; v < Agents.length; v++) {
+                if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag) {
+                flag = Math.min($(window).width(), $(window).height()) >= 600 ? true : false;
+            }
+            return flag;
+        }
         $(window).bind('resize', function () {
-            $('html').css('font-size', Math.min($(window).width(), $(window).height()) /(IsPC()?25:15) );
-            //$('title').text(Math.min($(window).width(), $(window).height()));
+            $('html').css('font-size', Math.min($(window).width(), $(window).height()) / (IsPC() ? 25 : 15));
         });
         $(window).trigger('resize');
     };
@@ -45,16 +43,15 @@ define(function (require, exports, module) {
         $(window).scroll(function (e) {
             var scrollTop = $(document).scrollTop();
             setNavPos(scrollTop);
-
         })
     };
-    function setNavPos(scrollTop){
-        var posY = component.header.height()-scrollTop;
+    function setNavPos(scrollTop) {
+        var posY = component.header.height() - scrollTop;
 
-        if(posY<=0){
+        if (posY <= 0) {
             component.nav.addClass('navFloat');
         }
-        else{
+        else {
             component.nav.removeClass('navFloat');
         }
     }
