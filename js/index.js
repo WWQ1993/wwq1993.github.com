b1a56df640bc2cc0f3dd4ce5170698ab53c6b966
WWQ1993 <516041710@qq.com>
2016-03-03 13:15:40 +0800

    test share

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ define(function (require, exports, module) {
             resizeCanvas();
         })
         $(window).resize();
-        initShare();
     };
     var initTimeLine = function () {
         var canvas = $('.timeLine')[0],
@@ -40,62 +39,5 @@ define(function (require, exports, module) {
         var canvas = $('.timeLine')
         canvas.width(canvas.parent().width());
         canvas.height(canvas.parent().width());
-    }
-
-    var initShare = function () {
-        var imgUrl = 'http://wwq1993.github.io/img/0.jpg';
-        var lineLink = 'http://www.jakehu.me/904';
-        var descContent = "这是晨曦的博客";
-        var shareTitle = '晨曦';
-        var appid = '';
-
-        function shareFriend() {
-            WeixinJSBridge.invoke('sendAppMessage',{
-                "appid": appid,
-                "img_url": imgUrl,
-                "img_width": "200",
-                "img_height": "200",
-                "link": lineLink,
-                "desc": descContent,
-                "title": shareTitle
-            }, function(res) {
-                //_report('send_msg', res.err_msg);
-            })
-        }
-        function shareTimeline() {
-            WeixinJSBridge.invoke('shareTimeline',{
-                "img_url": imgUrl,
-                "img_width": "200",
-                "img_height": "200",
-                "link": lineLink,
-                "desc": descContent,
-                "title": shareTitle
-            }, function(res) {
-                //_report('timeline', res.err_msg);
-            });
-        }
-        function shareWeibo() {
-            WeixinJSBridge.invoke('shareWeibo',{
-                "content": descContent,
-                "url": lineLink,
-            }, function(res) {
-                //_report('weibo', res.err_msg);
-            });
-        }
-        // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-        document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-            // 发送给好友
-            WeixinJSBridge.on('menu:share:appmessage', function(argv){
-                shareFriend();
-            });
-            // 分享到朋友圈
-            WeixinJSBridge.on('menu:share:timeline', function(argv){
-                shareTimeline();
-            });
-            // 分享到微博
-            WeixinJSBridge.on('menu:share:weibo', function(argv){
-                shareWeibo();
-            });
-        }, false);
     }
 })
